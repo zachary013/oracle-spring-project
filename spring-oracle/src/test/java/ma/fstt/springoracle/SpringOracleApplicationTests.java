abbda0b813d8b53427088255ad662986e24e537b
Zakariae Azn <azarkanzakariae@gmail.com>
2024-12-21 21:32:28 +0100

    First commit

---
+++ spring-oracle/src/test/java/ma/fstt/springoracle/SpringOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstt.springoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
